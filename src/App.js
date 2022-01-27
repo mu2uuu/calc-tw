860c3e1a31212650f56b3aa5742850a6276682ff
p-nuts5963 <polythene.pam5963@gimail.com>
2022-01-27 09:31:44 +0900

    add chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import DatePicker from "react-multi-date-picker";
-import "./App.css";
+import { Box, Container, Stack } from "@chakra-ui/react";
 
 function App() {
   const today = new Date();
@@ -9,13 +9,27 @@ function App() {
   const [values, setValues] = useState([today, tomorrow]);
   return (
     <>
-      <DatePicker
-        sort={true}
-        multiple
-        value={values}
-        onChange={setValues}
-        format="D"
-      />
+      <Container maxW="8xl" centerContent>
+        <Box
+          h={20}
+          w={"100%"}
+          backgroundColor={"orange.200"}
+          textAlign={"center"}
+        >
+          テレワークの日付簡単算出
+        </Box>
+        <Stack>
+          <Box mt="3">
+            <DatePicker
+              sort={true}
+              multiple
+              value={values}
+              onChange={setValues}
+              format="D"
+            />
+          </Box>
+        </Stack>
+      </Container>
     </>
   );
 }
