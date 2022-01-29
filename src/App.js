0a27313d4bd44c97404b5480fec81a89ca6e812c
p-nuts5963 <polythene.pam5963@gimail.com>
2022-01-30 00:37:37 +0900

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import DatePicker from "react-multi-date-picker";
-import { Box, Container, Stack, Select } from "@chakra-ui/react";
+import { Box, Container, Stack, Select, Center } from "@chakra-ui/react";
+import AppCss from "./App.css";
 import koyomi from "koyomi";
 
 function App() {
-  // 営業日マップ
-  const bizDay = [19, 18, 23, 20, 19, 22, 20, 22, 21, 20, 20, 22];
+  const containerStyle = {
+    height: "30px",
+  };
 
-  // 営業日
-  const biz = koyomi.biz("2022-1-1", "2022-1-31");
+  // 営業日マップ
+  const bizDayArray = [19, 18, 23, 20, 19, 22, 20, 22, 21, 20, 20, 22];
 
   // 年を取得
   const date = new Date();
@@ -20,66 +22,60 @@ function App() {
   tomorrow.setDate(tomorrow.getDate([today, tomorrow]));
   const [values, setValues] = useState([today, tomorrow]);
 
-  const [month, setMonth] = useState([]);
-  // const [bix, setBiz] = useState();
-  const handleMonth = (event) => {
-    const newMonth = event.target.value;
-    setMonth(newMonth);
-    console.log("year: " + year);
-    console.log("newMonth: " + newMonth);
-    // 営業日
-    const date = new Date(year, newMonth, 0);
+  const [selectedMonth, setSelectedMonth] = useState([]);
+  const [bizDay, setBizDay] = useState([]);
 
-    const startDay = year + "-" + newMonth + "-1";
-    const lastDay = year + "-" + newMonth + "-" + date.getDate();
-    const bizDay = koyomi.biz(startDay, lastDay);
-    console.log(newMonth + "月の営業日は" + bizDay);
+  const handleMonth = (event) => {
+    const newSelectedMonth = event.target.value;
+    setSelectedMonth(newSelectedMonth);
+
+    const newBizDay = bizDayArray[newSelectedMonth - 1];
+    setBizDay(newBizDay);
   };
   return (
     <>
-      <Container maxW="8xl" centerContent>
-        <Box
-          h={20}
-          w={"100%"}
-          backgroundColor={"orange.200"}
-          _
-          textAlign={"center"}
-        >
-          {year}年テレワークの日付簡単算出
-        </Box>
-        <Stack spacing={"5"}>
-          <Select
-            mt="3"
-            placeholder="月を選択"
-            onChange={(e) => handleMonth(e)}
-          >
-            <option value="1">1月</option>
-            <option value="2">2月</option>
-            <option value="3">3月</option>
-            <option value="4">4月</option>
-            <option value="5">5月</option>
-            <option value="6">6月</option>
-            <option value="7">7月</option>
-            <option value="8">8月</option>
-            <option value="9">9月</option>
-            <option value="10">10月</option>
-            <option value="11">11月</option>
-            <option value="12">12月</option>
-          </Select>
-          <Box>{month}月</Box>
-          <Box>営業日：{biz}</Box>
+      <Center h={10} backgroundColor={"orange.100"}>
+        {year}年テレワーク提出物のおもちゃ
+      </Center>
 
-          <Box mt="3">
-            <DatePicker
-              sort={true}
-              multiple
-              value={values}
-              onChange={setValues}
-              format="D"
-            />
-          </Box>
-        </Stack>
-      </Container>
+      <Stack spacing={"5"} align={"center"}>
+        <Select
+          mt="3"
+          placeholder="月を選択"
+          w="200px"
+          onChange={(e) => handleMonth(e)}
+        >
+          <option value="1">1月</option>
+          <option value="2">2月</option>
+          <option value="3">3月</option>
+          <option value="4">4月</option>
+          <option value="5">5月</option>
+          <option value="6">6月</option>
+          <option value="7">7月</option>
+          <option value="8">8月</option>
+          <option value="9">9月</option>
+          <option value="10">10月</option>
+          <option value="11">11月</option>
+          <option value="12">12月</option>
+        </Select>
+        <Box>
+          {selectedMonth}月の営業日は{bizDay}日です
+        </Box>
+        <Box mt="3">
+          <DatePicker
+            sort={true}
+            multiple
+            value={values}
+            onChange={setValues}
+            format="D"
+            style={{
+              height: "40px",
+              backgroundColor: "aliceblue",
+              padding: "10px",
+            }}
+          />
+        </Box>
+      </Stack>
     </>
   );
 }
